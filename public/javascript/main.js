44a79cc8338d5016572f45c73ca2e5cd29b0b1a4
aghataoliveira <aghatagoncalves@outlook.com>
2020-02-08 21:34:50 -0500

    fix: adding content

---
+++ public/javascript/main.js
@@ -0,0 +1,37 @@
+// borderBlur active
+var borderBlur = document.querySelectorAll("input[type=text], input[type=tel], input[type=email], textarea");
+borderBlur.forEach(function(field){
+  field.addEventListener("focus",function(){
+    this.classList.add ("borderBlur");
+  });
+});
+
+// remove borderBlur
+var borderBlur = document.querySelectorAll("input[type=text], input[type=tel], input[type=email], textarea");
+borderBlur.forEach(function(field){
+  field.addEventListener("blur",function(){
+    this.classList.remove ("borderBlur");
+  });
+});
+// Menu
+// Show sticky header on click
+var burguerMenu = document.getElementsByClassName("burguerMenu")[0];
+burguerMenu.addEventListener("click", function(myFunction){
+  var stickyHeader = document.getElementsByClassName("sticky-header")[0];
+if (stickyHeader.style.display == "block"){
+    stickyHeader.style.display = "none";
+}else{
+  stickyHeader.style.display = "block";
+}
+});
+// Hide sticky header on click
+var burguerMenu = document.getElementsByClassName("sticky-header")[0];
+burguerMenu.addEventListener("click", function(myFunction){
+  if (window.innerWidth>650){
+    var stickyHeader = document.getElementsByClassName("sticky-header")[0];
+      stickyHeader.style.display = "block";
+  }else{
+    var stickyHeader = document.getElementsByClassName("sticky-header")[0];
+    stickyHeader.style.display = "none";
+  }
+});
